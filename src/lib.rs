5e4f7f18da00f831e7c6c58e2e02bce7127b9207
Ian Mason <dormedas@vivaldi.net>
2019-12-04 20:36:30 -0800

    Create common module Extract file operations from day one module.

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,45 @@ mod tests {
     }
 }
 
+pub mod common {
+    use std::fs::File;
+    // For read_to_string below
+    use std::io::Read;
+
+    pub fn read_file_to_string(file_name: &str) -> String {
+        let mut f = File::open(file_name).unwrap();
+        let mut contents = String::new();
+        f.read_to_string(&mut contents).unwrap();
+
+        contents
+    }
+}
+
 pub mod one {
+    pub fn run() {
+        let contents: String = super::common::read_file_to_string("one_input.txt");
+
+        let masses_str: Vec<&str> = contents.split('\n').collect();
+
+        let mut masses: Vec<i64> = Vec::new();
+
+        for i in &masses_str {
+            if i.is_empty() {
+                continue;
+            }
+            let converted: i64 = i64::from_str_radix(i, 10).unwrap();
+            masses.push(converted);
+        }
+
+
+        let mut fuel_requirement_total: i64 = 0;
+        for i in &masses {
+            fuel_requirement_total += generate_fuel_for_module(*i);
+        }
+
+        println!("{}", fuel_requirement_total);
+    }
+
     pub fn generate_fuel_for_module(mass: i64) -> i64 {
         let mass_divided: i64 = mass / 3;
         let result = mass_divided - 2;
