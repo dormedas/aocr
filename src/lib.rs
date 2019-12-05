97bc807fccdf1e85b882053dc11b128b5d965060
Ian Mason <dormedas@vivaldi.net>
2019-12-04 20:16:09 -0800

    Day 1.5 solved

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,12 @@ mod tests {
 pub mod one {
     pub fn generate_fuel_for_module(mass: i64) -> i64 {
         let mass_divided: i64 = mass / 3;
-        mass_divided - 2
+        let result = mass_divided - 2;
+        if result <= 0 {
+            0
+        } else {
+            let fuel_mass: i64 = generate_fuel_for_module(result);
+            result + fuel_mass
+        }
     }
 }
--- tests/one_test.rs
+++ tests/one_test.rs
@@ -2,9 +2,7 @@ use days::one::*;
 
 #[test]
 fn verify_fuel_calculation() {
-    assert_eq!(generate_fuel_for_module(12), 2);
     assert_eq!(generate_fuel_for_module(14), 2);
-    assert_eq!(generate_fuel_for_module(1969), 654);
-    assert_eq!(generate_fuel_for_module(100756), 33583);
-
+    assert_eq!(generate_fuel_for_module(1969), 966);
+    assert_eq!(generate_fuel_for_module(100756), 50346);
 }
